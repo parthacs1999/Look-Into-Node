5e1ae27a0fbf90e4c074cd0865dc2f029e86a4f9
Partha Chakraborty <partha.c1999@gmail.com>
2023-04-01 18:26:33 +0530

    Parsing incoming request logic added and uses of express-router also added

--- Express_Starter/app.js
+++ Express_Starter/app.js
@@ -1,7 +1,39 @@
-const http=require('http');
+// const http=require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+// app.use((req,res,next)=>{
+//     console.log('In the middleware');
+//     next();//Allows the request to continue to the next middleware
+// });
 
-const express=require('express');
-const app=express();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-const server=http.createServer(app);
-server.listen(3000);
+// const server=http.createServer(app);
+// server.listen(3000);
+app.listen(3000);
+
+
+
+
+// Assignment
+
+// const express=require('express');
+// const app=express();
+// app.use('/users',(req,res,send)=>{
+//     console.log("First middleware");
+//     res.send('<h1>Users Data</h1>');
+// })
+// app.use('/',(req,res,send)=>{
+//     console.log("second middleware");
+//     res.send('<h1>Home Page</h1>');
+// })
+// app.listen(3000);
+
+
+// app.post--->only for post request
+// app.get--->only for get request
