8baeba2f17d7d66152aec01acd8629581fde6a8c
Partha Chakraborty <partha.c1999@gmail.com>
2023-04-04 00:21:57 +0530

    Explored new express topic like filtering paths,serving HTML pages,Serving files statically etc.

--- Express_Starter/app.js
+++ Express_Starter/app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const path=require('path');
 // app.use((req,res,next)=>{
 //     console.log('In the middleware');
 //     next();//Allows the request to continue to the next middleware
@@ -10,9 +11,11 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
-
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+})
 // const server=http.createServer(app);
 // server.listen(3000);
 app.listen(3000);
