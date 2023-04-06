7a3344747890f4f8adf73bca1712041b01a1182b
Partha Chakraborty <partha.c1999@gmail.com>
2023-04-06 23:04:47 +0530

    Added templating engine logic using PUG

--- Express_Starter/routes/admin.js
+++ Express_Starter/routes/admin.js
@@ -6,18 +6,19 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
-const products=[];
+const products = [];
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', { pageTitle: 'Add Product',path:'/admin/add-product' })
 });
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  products.push({title:req.body.title});
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
 // module.exports = router;
-exports.routes=router;
-exports.products=products;
+exports.routes = router;
+exports.products = products;
--- Express_Starter/routes/shop.js
+++ Express_Starter/routes/shop.js
@@ -8,8 +8,10 @@ const adminData=require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  console.log(adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  // console.log(adminData.products);
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products=adminData.products;
+  res.render('shop', { prods: products, docTitle: 'Shop', path: '/', pageTitle :'Shop'});
 });
 
 module.exports = router;
