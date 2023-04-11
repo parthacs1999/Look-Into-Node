b3502cc11705c595789c93ad7ea329f951b62a31
Partha Chakraborty <partha.c1999@gmail.com>
2023-04-11 16:43:24 +0530

    getting product id from json,fetching data from json with particular id,dynamic routes logic added

--- MVC/routes/shop.js
+++ MVC/routes/shop.js
@@ -3,11 +3,21 @@ const path = require('path');
 const express = require('express');
 
 const shopController = require('../controllers/shop');
+
 const router = express.Router();
 
 router.get('/', shopController.getIndex);
-router.get('/products',shopController.getProducts);
-router.get('/cart',shopController.getCart);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/products/:productId',shopController.getProduct)
+
+router.get('/cart', shopController.getCart);
+
+router.post('/cart',shopController.postCart);
+
 router.get('/orders', shopController.getOrders);
-router.get('/checkout',shopController.getCheckout);
+
+router.get('/checkout', shopController.getCheckout);
+
 module.exports = router;
